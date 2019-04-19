f0ebaa26c45d4bb6304c1f8737f07e8eec577ce6
Lior-Saadon <lior.saadon@mail.huji.ac.il>
2019-04-19 17:43:02 +0300

    basic structure for our app - bottom navigation menu with fragments

--- app/src/main/java/com/example/bookvibez/MainActivity.java
+++ app/src/main/java/com/example/bookvibez/MainActivity.java
@@ -1,43 +1,69 @@
 package com.example.bookvibez;
 
 import android.os.Bundle;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_map_view:
-                    mTextMessage.setText(R.string.title_map_view);
-                    return true;
-                case R.id.navigation_list:
-                    mTextMessage.setText(R.string.title_list);
-                    return true;
-                case R.id.navigation_profile:
-                    mTextMessage.setText(R.string.title_profile);
-                    return true;
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        mTextMessage = findViewById(R.id.message);
+        //loading the default fragment
+        loadFragment(new WelcomeFragment());
+
+        //getting bottom navigation view and attaching the listener
+        BottomNavigationView navView = findViewById(R.id.navigation);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+
+
+    /*
+     * implementation of the listener to the bottom navigation menu.
+     */
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment = null;
+            switch (item.getItemId()) {
+                case R.id.navigation_map_view:
+                    fragment = new MapFragment();
+                    break;
+                case R.id.navigation_list:
+                    fragment = new ListFragment();
+                    break;
+                case R.id.navigation_profile:
+                    fragment = new ProfileFragment();
+                    break;
+            }
+            return loadFragment(fragment);
+        }
+    };
+
+
+    /*
+     * this method is switching fragments.
+     * @param fragment - a Fragment typed object
+     * @return true if switched successfully.
+     */
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
 
 }
