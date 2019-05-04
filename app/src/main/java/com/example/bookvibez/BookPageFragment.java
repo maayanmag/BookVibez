b8d1ab28c9844730d40fb0a97991b712b93f2f9e
Lior-Saadon <lior.saadon@mail.huji.ac.il>
2019-05-04 15:40:06 +0300

    connected bookPage

--- app/src/main/java/com/example/bookvibez/BookPageFragment.java
+++ app/src/main/java/com/example/bookvibez/BookPageFragment.java
@@ -4,15 +4,28 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
-public class BookPageFragment extends Fragment {
+public class BookPageFragment extends Fragment implements View.OnClickListener {
+
+    public static BookItem bookToDisplay = null;
+
+    private CollapsingToolbarLayout collapsingToolbar;
+    private ImageView bookImg, ownerImg;
+    private TextView name, author, genre, owner;
+    private Button gotBookButton;
+    private boolean isPressed = false;
+
+
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Nullable
     @Override
@@ -20,20 +33,42 @@ public class BookPageFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_book_page, container, false);
 
-        ImageView img = (ImageView) view.findViewById(R.id.current_owner_profile_pic);
-        img.setClipToOutline(true);
+        getAttrinutesIds(view);
+        handlingFloatingButton(view);
 
-       // handlingFloatingButton(view);
-
-        /* CollapsingToolbarLayout collapsingToolbar =
-                (CollapsingToolbarLayout) view.findViewById(R.id.collapsing_toolbar);
-        collapsingToolbar.setTitle("Book Name"); */
+        if(bookToDisplay != null)
+            handleAttribute();
 
         return view;
-
-
     }
 
+
+    private void getAttrinutesIds(View view) {
+        bookImg = (ImageView) view.findViewById(R.id.toolbar_image);
+        ownerImg = (ImageView) view.findViewById(R.id.current_owner_profile_pic);
+
+        name = (TextView) view.findViewById(R.id.book_name_content);
+        author = (TextView) view.findViewById(R.id.book_author_content);
+        genre = (TextView) view.findViewById(R.id.book_genre_content);
+        owner = (TextView) view.findViewById(R.id.current_owner_name);
+
+        collapsingToolbar = (CollapsingToolbarLayout) view.findViewById(R.id.collapsing_toolbar);
+
+        gotBookButton = (Button) view.findViewById(R.id.got_the_book_button);
+        gotBookButton.setOnClickListener(this);
+    }
+
+    private void handleAttribute() {
+        name.setText(bookToDisplay.getTitle());
+        author.setText(bookToDisplay.getAuthor());
+        genre.setText(bookToDisplay.getGenre());
+        owner.setText(bookToDisplay.getOwnerName());
+
+        bookImg.setImageResource(bookToDisplay.getBookImg());
+        ownerImg.setImageResource(bookToDisplay.getOwnerImg());
+
+        collapsingToolbar.setTitle(bookToDisplay.getTitle());
+    }
 
     /**
      * the function handles the Add floating button object in content_scrolling_list.
@@ -52,7 +87,19 @@ public class BookPageFragment extends Fragment {
     }
 
 
-
-
-
+    @Override
+    public void onClick(View v) {
+        isPressed = !isPressed;     // change state
+        String text;
+        if(isPressed) {
+            text = "I don't own it";
+            gotBookButton.setBackgroundResource(R.drawable.got_the_book_button_pressed);
+            //TODO - change current owner to this user
+        }
+        else {
+            text = "I Got This Book!";
+            gotBookButton.setBackgroundResource(R.drawable.got_the_book_button_unpressed);
+        }
+        gotBookButton.setText(text);
+    }
 }
